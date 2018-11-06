14fc55f555e390bcae484ef00435c9ec67372c95
Oriol Ylla <uriylla@gmail.com>
2018-11-06 01:11:42 +0100

    fix stor creation error

--- src/store.js
+++ src/store.js
@@ -4,7 +4,9 @@ import cardsReducer from './reducers/cards'
 const store = createStore(
   combineReducers({
     cardsReducer
-  })
+  }),
+  [],
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default store;
