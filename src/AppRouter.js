dd1e420728c39c6f8dbae80a107d78ad1d23e973
Oriol Ylla <uriylla@gmail.com>
2018-11-14 18:33:10 +0100

    add route added. Header and Modal moved to blocks.

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 
-import Header from './components/Header';
+import Header from './blocks/Header';
 import Deck from './components/Deck';
+import CardView from './components/CardView';
+import Modal from './blocks/Modal';
 
 export const history = createHistory()
+
+const addCard = (props) =>
+<Modal>
+  <CardView 
+    text={'Some Card'}
+    pick={1}
+    adding={true}/>
+</Modal>
 
 const AppRouter = () => (
   <Router history={history}>
     <div>
       <Header > Card  game maker </Header>
       <Route path="/" component={Deck}/>
+      <Route path="/add" render={addCard}/>
     </div>
   </Router>
 );
